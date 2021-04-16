83abc519cb394e268b816ef375b7be0996a3967f
Miguel Flores <mky.flores@hotmail.com>
2021-04-16 13:27:32 -0500

    Added files and project structure

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args) {
+        FileHandler fileHandler = new FileHandler("src/testData.txt");
+        System.out.println(fileHandler.getLine(0));
+    }
+
+
+    public static void formatLine(String line) {
+        int rabbitPercent = 0;
+        int grassPerfect = 0;
+
+        //      2.7%          0%        20.9%
+        System.out.println();
+    }
+}
